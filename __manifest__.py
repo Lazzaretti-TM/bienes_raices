0e488d9d6ffc845fab6d9bfd78732592a94feffa
Lazzaretti-TM <morenotianny@gmail.com>
2026-03-15 13:54:22 -0400

    Add manifest in models

--- __manifest__.py
+++ __manifest__.py
@@ -1,10 +1,11 @@
 {
     "name": "Bienes Raices",
     "sumary": "Modulo para la gestion de bienes y raices",
-    "version": "19.0.1.0.0",
+    "version": "19.0.1.0.1",
     "license": "AGPL-3",
     "autor": "Moreno",
-    "aplication": True,
+    "application": True,
+    
     
     
 }
