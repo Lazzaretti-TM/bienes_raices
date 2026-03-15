3ebe4d8c165f7d7dad6a5490768516039e3dc6dc
Lazzaretti-TM <morenotianny@gmail.com>
2026-03-15 12:19:42 -0400

    Add Manifest in module

---
+++ __manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name": "Bienes Raices",
+    "sumary": "Modulo para la gestion de bienes y raices",
+    "version": "19.0.1.0.0",
+    "license": "AGPL-3",
+    "autor": "Moreno",
+    "aplication": True,
+    
+    
+}
