66843cbeae232e6f1b489bfada5cbaaa14b5ec18
Lazzaretti-TM <morenotianny@gmail.com>
2026-03-22 13:39:58 -0400

    Add manifest in  models

--- __manifest__.py
+++ __manifest__.py
@@ -4,8 +4,12 @@
     "version": "19.0.1.0.1",
     "license": "AGPL-3",
     "autor": "Moreno",
+    "data":[
+        "security/ir.model.access.csv",
+        "views/views.xml",
+        "data/real_estate_menus.xml"
+    ],
     "application": True,
-    
-    
+        
     
 }
